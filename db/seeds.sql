976fcd0f38364c15c475283da21f1d83c291cfc6
Karin Sundin <88103504+sundinkarin@users.noreply.github.com>
2023-05-05 11:02:50 -0400

    seeded program

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,14 +7,14 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES 
-       ("Human Resources Manager", 70000, 01),
+       ("Human Resources Manager", 100000, 01),
        ("Recruiter", 60000, 01);
        ("Junior Software Developer", 70000, 02),
        ("Senior Software Developer", 125000, 02),
        ("Accountant", 150000, 03),
        ("Lawyer", 300000, 03),
        ("Sales Manager", 300000, 04),
-       ("General Manager", 80000, 04);
+       ("General Manager", 800000, 04);
        ("Payroll Manager", 200000, 04),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
